d7a21cddc6637b219ed8e201131d7ed2065fd200
Taccu <HolyFaith@web.de>
2016-09-21 21:48:18 +0200

    added implemasdikojasohdidiosjkaggiuafsd
    now works

--- app/src/main/java/de/dhbw/bluebacon/model/Tuple.java
+++ app/src/main/java/de/dhbw/bluebacon/model/Tuple.java
@@ -1,11 +1,12 @@
 package de.dhbw.bluebacon.model;
 
+import java.util.Comparator;
 /**
  * Tuple class for coordinates
  * @param <X> Data type of X coordinate
  * @param <Y> Data type of Y coordinate
  */
-public class Tuple<X, Y> {
+public class Tuple<X, Y> implements Comparator<Tuple>{
     protected final X x;
     protected final Y y;
 
@@ -20,5 +21,22 @@ public class Tuple<X, Y> {
 
     public Y getY() {
         return this.y;
+    }
+    
+    @Override
+    public int compare(Tuple o1, Tuple o2) {
+        if(o1.x == o2.x && o1.y == o2.y) {
+            return 0;
+        }
+        return 1;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Tuple) {
+            Tuple<X,Y> asd = (Tuple<X,Y>) obj;
+            return asd.x == this.x && asd.y == this.y;
+        }
+        return false;
     }
 }
